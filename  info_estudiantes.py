f7117d050760f733dbcafde0afa72df3f5bada28
VACOX <162767087+VACOX@users.noreply.github.com>
2026-03-04 01:19:52 +0000

    Creada funcion nombres_estudiantes

---
+++  info_estudiantes.py
@@ -0,0 +1,8 @@
+def nombres_estudiantes ():
+    estudiante_propietario= "Justin Soledispa"
+    estudiante_colaborador= "Daniel Vaca"
+    print(f"Estudiante propietario: {estudiante_propietario}")
+    print(f"Estudiante colaborador: {estudiante_colaborador}")  
+
+#Main
+nombres_estudiantes()
