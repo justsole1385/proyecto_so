7e7b0b0292cb4fe96d78b630090375153a161396
justsole1385 <soledj1325@gmail.com>
2026-03-04 14:36:52 +0000

    Se crea archivo info_proyecto con descripcion

---
+++ info_proyecto.py
@@ -0,0 +1,5 @@
+def descripcion_proyecto():
+    print("Descripción del Proyecto:")
+    print("Proyecto Final de la materia UCOM 418 - Sistemas Operativos (Paralelo 81)")
+    print("Tema del Proyecto:")
+    print("Simulación de una plataforma donde usuarios reservan asientos en una sala de cine, garantizando la correcta asignación y control de disponibilidad.")
